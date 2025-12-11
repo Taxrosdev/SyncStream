fd38a003adf125febfba500a0e9bba92850f7f81
Taxrosdev <taxrosdev@mailbox.org>
2025-12-11 23:24:18 +1300

    fix: reimpl majority of hash
    I'm not quite sure what I was thinking initially when prematurely commiting the previous commit...
    
    Changes: Add tests, Switch from string inputs (???) to enums, Remove output sizes (xxh3 doesn't like them)

--- src/hash.rs
+++ src/hash.rs
@@ -1,54 +1,70 @@
 use xxhash_rust::xxh3::Xxh3;
 
-pub const DEFAULT_HASH: &str = "blake3";
-
 pub enum Hasher {
     Blake3 { hasher: Box<blake3::Hasher> },
-    Xxh3 { hasher: Box<Xxh3>, len: OutputSize },
+    Xxh3 { hasher: Box<Xxh3> },
 }
 
-pub enum OutputSize {
-    Size64,
-    Size128,
+#[derive(Default, Clone, Copy, PartialEq, Debug)]
+pub enum HashKind {
+    #[default]
+    Blake3,
+    Xxh3,
 }
 
 impl Hasher {
-    /// Returns `None` if nothing matches
-    pub fn new(name: &str) -> Option<Self> {
-        match name.to_ascii_lowercase().as_str() {
-            "blake3" => Some(Self::Blake3 {
+    pub fn new(kind: HashKind) -> Self {
+        match kind {
+            HashKind::Blake3 => Self::Blake3 {
                 hasher: Box::new(blake3::Hasher::new()),
-            }),
-            "xxh3_64" => Some(Self::Xxh3 {
+            },
+            HashKind::Xxh3 => Self::Xxh3 {
                 hasher: Box::new(Xxh3::new()),
-                len: OutputSize::Size64,
-            }),
-            "xxh3_128" => Some(Self::Xxh3 {
-                hasher: Box::new(Xxh3::new()),
-                len: OutputSize::Size128,
-            }),
-            _ => None,
+            },
         }
     }
-
-    pub fn new_default() -> Self {}
 
     pub fn update(&mut self, data: &[u8]) {
         match self {
             Hasher::Blake3 { hasher } => {
                 hasher.update(data);
             }
-            Hasher::Xxh3 { hasher, .. } => hasher.update(data),
+            Hasher::Xxh3 { hasher } => {
+                hasher.update(data);
+            }
         };
     }
 
     pub fn finalize(self) -> String {
         match self {
-            Hasher::Blake3 { hasher } => hasher.finalize().to_hex().to_string(),
-            Hasher::Xxh3 { hasher, len } => match len {
-                OutputSize::Size64 => hex::encode(hasher.digest().to_le_bytes()),
-                OutputSize::Size128 => hex::encode(hasher.digest128().to_le_bytes()),
-            },
+            Self::Blake3 { hasher } => hasher.finalize().to_hex().to_string(),
+            Self::Xxh3 { hasher } => hex::encode(hasher.digest().to_le_bytes()),
         }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_blake3_stability() {
+        let mut hasher = Hasher::new(HashKind::Blake3);
+
+        hasher.update("Test Input".as_bytes());
+
+        assert_eq!(
+            hasher.finalize(),
+            "333ea0accb9fbcab70b14d102ef9c98c9df7060a72c95b46322aa8ad09a6ec51".to_string()
+        );
+    }
+
+    #[test]
+    fn test_xxh3_stability() {
+        let mut hasher = Hasher::new(HashKind::Xxh3);
+
+        hasher.update("Test Input".as_bytes());
+
+        assert_eq!(hasher.finalize(), "9ecc4f8d8238ad80".to_string());
     }
 }
