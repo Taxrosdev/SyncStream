b729964df8c7a21be408c42b89bedf2fcbddaa30
Taxrosdev <taxrosdev@mailbox.org>
2025-12-29 21:09:09 +1300

    refactor: switch to thiserror

--- src/error.rs
+++ src/error.rs
@@ -1,36 +1,12 @@
 pub type Result<T> = std::result::Result<T, Error>;
 
-#[derive(Debug)]
+#[derive(thiserror::Error, Debug)]
 pub enum Error {
-    IoError(std::io::Error),
-    NetworkError(reqwest::Error),
+    #[error("io error: {0:?}")]
+    IoError(#[from] std::io::Error),
+    #[error("network error: {0:?}")]
+    NetworkError(#[from] reqwest::Error),
     /// Expected and Recieved
+    #[error("hash error: expected {0}, got {1}")]
     HashError(String, String),
-}
-
-impl std::fmt::Display for Error {
-    fn fmt(&self, formatter: &mut std::fmt::Formatter) -> std::result::Result<(), std::fmt::Error> {
-        match self {
-            Error::IoError(err) => write!(formatter, "io error: {err}"),
-            Error::NetworkError(err) => write!(formatter, "network error: {err}"),
-            Error::HashError(expected, recieved) => write!(
-                formatter,
-                "hash error: expected {expected}, recieved {recieved}"
-            ),
-        }
-    }
-}
-
-impl std::error::Error for Error {}
-
-impl From<std::io::Error> for Error {
-    fn from(err: std::io::Error) -> Self {
-        Self::IoError(err)
-    }
-}
-
-impl From<reqwest::Error> for Error {
-    fn from(err: reqwest::Error) -> Self {
-        Self::NetworkError(err)
-    }
 }
